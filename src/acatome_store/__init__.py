27da277a42880aca012b6889f97d77d0cdd9ab82
Reto Stamm <reto@retostamm.com>
2026-03-11 11:25:05 +0000

    Bump version: 0.1.0 → 0.2.0

--- src/acatome_store/__init__.py
+++ src/acatome_store/__init__.py
@@ -5,4 +5,4 @@ from acatome_store.store import Store
 from acatome_store.vector import VectorIndex, ChromaIndex, create_index
 
 __all__ = ["Store", "Ref", "VectorIndex", "ChromaIndex", "create_index"]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
